17612996894e49b761859f14a04179656a2cabba
DjokaraS <djordje.lfc@gmail.com>
2021-11-18 06:03:57 +0100

    Fix to accordion functionality

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 
 const arrow = document.querySelectorAll(`.icon`);
 const card = document.querySelectorAll(`.card`);
+const acc = document.querySelector(`.accordion`);
 
 for (let i = 0; i < arrow.length; i++) {
 	arrow[i].addEventListener(`click`, function () {
-		card[i].classList.toggle(`active`);
+		for (let l = 0; l < card.length; l++) {
+			if (card[l].classList.contains(`active`)) {
+				card[l].classList.remove(`active`);
+			}
+		}
+		card[i].classList.add(`active`);
 	});
 }
