60c4e2e4734b6e951da5abca1068dd047e8046bc
DjokaraS <djordje.lfc@gmail.com>
2021-11-22 04:46:50 +0100

    mobile and desktop + readme

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const arrow = document.querySelectorAll(`.icon`);
+const topPart = document.querySelectorAll(`.top-part`);
 const card = document.querySelectorAll(`.card`);
 const acc = document.querySelector(`.accordion`);
 
-for (let i = 0; i < arrow.length; i++) {
-	arrow[i].addEventListener(`click`, function () {
+for (let i = 0; i < topPart.length; i++) {
+	topPart[i].addEventListener(`click`, function () {
 		for (let l = 0; l < card.length; l++) {
 			if (card[l].classList.contains(`active`)) {
 				card[l].classList.remove(`active`);
